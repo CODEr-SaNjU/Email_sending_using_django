c9bd202297b785206554cabb7bf8caa99187f20c
CoDEr-SaNjU <sanju2help@gmail.com>
2020-01-20 19:05:13 +0530

    show/hide file browser

--- main_project/backend/forms.py
+++ main_project/backend/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 class EmailForm(forms.Form):
     CHOICES=(
-        ("1","Yes"),
-        ("2","No"),
+        ("show","Yes"),
+        ("Hide","No"),
     )
     email = forms.EmailField()
     subject = forms.CharField(max_length=100, required=True)
     option = forms.ChoiceField(choices = CHOICES, widget=forms.RadioSelect(attrs={'name':'option'}),label='If you want send a file')
-    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'id':'option'}),required=True)
+    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'id':'optionchange',}),required=False)
     message = forms.CharField(widget=forms.Textarea)
 
 
