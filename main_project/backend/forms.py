f858358298fb1899ed49025ff50391e92a48f9e2
CoDEr-SaNjU <sanju2help@gmail.com>
2020-02-03 14:37:54 +0530

    implement some function

--- main_project/backend/forms.py
+++ main_project/backend/forms.py
@@ -1,13 +1,10 @@
 from django import forms
 class EmailForm(forms.Form):
-    CHOICES=(
-        ("show","Yes"),
-        ("Hide","No"),
-    )
-    email = forms.EmailField()
+    email = forms.EmailField(label="email")
+    Verify = forms.EmailField(label="verify email",required=True)
+    Designation = forms.CharField(max_length=100,required=True)
+    project = forms.CharField(max_length=100,required=True)
     subject = forms.CharField(max_length=100, required=True)
-    option = forms.ChoiceField(choices = CHOICES, widget=forms.RadioSelect(attrs={'name':'option'}),label='If you want send a file')
-    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True,'id':'optionchange',}),required=False,label=False)
     message = forms.CharField(widget=forms.Textarea)
 
 
--- main_project/backend/views.py
+++ main_project/backend/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage , EmailMultiAlternatives
 from django.conf import settings
 from  . forms import EmailForm
 from django.conf import settings
-
+from django.template.loader import get_template,render_to_string
+from backend.utils import render_to_pdf
 class EmailAttachementView(View):
     form_class = EmailForm
     template_name = 'html_files/Sender.html'
@@ -16,23 +17,30 @@ class EmailAttachementView(View):
         return render(request,self.template_name,{'email_form':form})
     
     def post(self,request, *args, **kwargs):
-        form = self.form_class(request.POST,request.FILES)
+        form = self.form_class(request.POST)
         if form.is_valid():
-            email   = form.cleaned_data['email']
+            to  = form.cleaned_data['email']
+            Designation = form.cleaned_data['Designation']
+            project = form.cleaned_data['project']
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
-            option = form.cleaned_data['option']
-            files = request.FILES.getlist('attach')
+            Verify = form.cleaned_data['Verify']
+            context = {
+                'to':to,
+                'Designation':Designation,
+                'project':project,
+                'subject':subject,
+                'message':message,
+                }
+            html_template = get_template('html_files/Reciver.htm')
+            html_Content = html_template.render(context)
             try:
-                mail = EmailMessage(subject,message,settings.EMAIL_HOST_USER,[email])
-                if files:
-                    for f in files:
-                        mail.attach(f.name,f.read(),f.content_type)
-                        mail.send()
-                else:
-                    mail.send()
+                msg = EmailMultiAlternatives(subject,html_Content,settings.EMAIL_HOST_USER,[to])
+                msg.attach_alternative(html_Content,"text/html")
+                # msg.attach_alternative(pdf,"application/pdf")
+                msg.send()
                 return render(request,self.template_name,{'email_form':form,'error_message':'sent Email to %s'%email})
             except:
-                return render(request,self.template_name,{'email_form':form,'error_message':'Either the attachment is too big or corrupt '})
+                return render(request,self.template_name,{'email_form':form,})
         return render(request, self.template_name, {'email_form': form, 'error_message': 'Unable to send email. Please try again later'})             
 
