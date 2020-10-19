af225e31398a560a962bc9c8888b8c1f52da88d8
coder-sanju <sanju2help@gmail.com>
2020-10-19 21:08:44 +0530

    update

--- main_project/backend/urls.py
+++ main_project/backend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/compose', views.EmailAttachementView.as_view(),name='emailattachementview'),
+    path('compose/', views.EmailAttachementView.as_view(),name='emailattachementview'),
     path('',views.Home_page,name="Home_page")
 ]
